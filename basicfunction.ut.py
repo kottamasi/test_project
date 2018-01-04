59b750f63e2562bf5c3df61512b2de006131dfbd
moshe <kottamasi.moshe@votarytech.com>
2018-01-04 11:58:39 +0530

    my geetha

--- basicfunction.ut.py
+++ basicfunction.ut.py
@@ -3,9 +3,11 @@ import unittest
 class TestBasicFunction(unittest.TestCase):
     def test_1(self):
         self.assertTrue(True)
+       
  
     def test_2(self):
         self.assertTrue(True)
- 
+	
+
 if __name__ == '__main__':
     unittest.main()
